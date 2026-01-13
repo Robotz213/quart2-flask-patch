0b89e3cfb53e0ecb319dfb002bbd72aabee3244f
Nicholas Silva <nicholasazs213@gmail.com>
2026-01-13 14:15:59 -0400

    Remove asyncio patching functions from _patch.py

--- src/quart_flask_patch/_patch.py
+++ src/quart_flask_patch/_patch.py
@@ -1,84 +1,11 @@
 from __future__ import annotations
 
-import asyncio
 import inspect
 import sys
 import types
 from typing import Any, Callable
 
 from ._synchronise import sync_with_context
-
-
-def _patch_asyncio() -> None:
-    # This patches asyncio to add a sync_wait method to the event
-    # loop. This method can then be called from within a task
-    # including a synchronous function called from a task. Sadly it
-    # requires the python Task and Future implementations, which
-    # invokes some performance cost.
-    asyncio.Task = asyncio.tasks._CTask = asyncio.tasks.Task = asyncio.tasks._PyTask  # type: ignore
-    asyncio.Future = (  # type: ignore
-        asyncio.futures._CFuture  # type: ignore
-    ) = (
-        asyncio.futures.Future  # type: ignore
-    ) = asyncio.futures._PyFuture  # type: ignore
-
-    current_policy = asyncio.get_event_loop_policy()
-    if hasattr(asyncio, "unix_events"):
-        target_policy = asyncio.unix_events._UnixDefaultEventLoopPolicy
-    else:
-        target_policy = object  # type: ignore
-
-    if not isinstance(current_policy, target_policy):
-        raise RuntimeError("Flask Patching only works with the default event loop policy")
-
-    _patch_loop()
-    _patch_task()
-
-
-def _patch_loop() -> None:
-    def _sync_wait(self, future):  # type: ignore
-        preserved_ready = list(self._ready)
-        self._ready.clear()
-        current_task = asyncio.tasks._current_tasks.get(self)  # type: ignore[attr-defined]
-        future = asyncio.tasks.ensure_future(future, loop=self)
-        while not future.done() and not future.cancelled():
-            self._run_once()
-            if self._stopping:
-                break
-            if current_task._must_cancel:
-                future.cancel()
-        self._ready.extendleft(preserved_ready)
-        return future.result()
-
-    asyncio.BaseEventLoop.sync_wait = _sync_wait  # type: ignore
-
-
-def _patch_task() -> None:
-    # Patch the asyncio task to allow it to be re-entered.
-    def enter_task(loop, task):  # type: ignore
-        asyncio.tasks._current_tasks[loop] = task  # type: ignore[attr-defined]
-
-    asyncio.tasks._enter_task = enter_task
-
-    def leave_task(loop, task):  # type: ignore
-        del asyncio.tasks._current_tasks[loop]  # type: ignore[attr-defined]
-
-    asyncio.tasks._leave_task = leave_task
-
-    def step(self, exception=None):  # type: ignore
-        current_task = asyncio.tasks._current_tasks.get(self._loop)  # type: ignore[attr-defined]
-        try:
-            self._Task__step_orig(exception)
-        finally:
-            if current_task is None:
-                asyncio.tasks._current_tasks.pop(self._loop, None)  # type: ignore[attr-defined]
-            else:
-                asyncio.tasks._current_tasks[  # type: ignore[attr-defined]
-                    self._loop
-                ] = current_task
-
-    asyncio.Task._Task__step_orig = asyncio.Task._Task__step  # type: ignore
-    asyncio.Task._Task__step = step  # type: ignore
 
 
 def _context_decorator(func: Callable) -> Callable:
@@ -115,5 +42,4 @@ def _patch_modules() -> None:
 
 
 def patch_all() -> None:
-    _patch_asyncio()
     _patch_modules()
